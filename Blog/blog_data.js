5ea4d5dcfec35584e4bba1a5f1432068fb7f2165
Jackson Kenneth Clayton <59662161+JKClaytonDev@users.noreply.github.com>
2022-01-14 06:08:34 -0500

    Add files via upload

---
+++ Blog/blog_data.js
@@ -0,0 +1,86 @@
+let posts = [];
+let str;
+
+str = `
+<h1>Hurricane Dorian</h1>
+<p>
+Hurricane Dorian was the strongest hurricane to affect the Bahamas on
+record, causing catastrophic damage in the Abaco Islands and Grand
+Bahama in early September 2019. The cyclone's intensity as well as its
+slow forward motion near the Bahamas broke numerous records. The fifth
+tropical cyclone, fourth named storm, second hurricane, and first
+major hurricane of the 2019 Atlantic hurricane season, Dorian
+originated from a westward-traveling tropical wave that departed from
+the western coast of Africa on August 19. The system organized into a
+tropical depression and later a tropical storm, both on August 24.
+</p>`;
+
+posts.push(str);
+
+str = `
+<h1>Dermotherium</h1>
+<p>
+Dermotherium is a genus of fossil mammals closely related to the
+living colugos, a small group of gliding mammals from Southeast
+Asia. Two species are recognized: D. major from the Late Eocene of
+Thailand, based on a single fragment of the lower jaw, and D. chimaera
+from the Late Oligocene of Thailand, known from three fragments of the
+lower jaw and two isolated upper molars. In addition, a single
+isolated upper molar from the Early Oligocene of Pakistan has been
+tentatively assigned to D. chimaera. All sites where fossils of
+Dermotherium have been found were probably forested environments and
+the fossil species were probably forest dwellers like living colugos,
+but whether they had the gliding adaptations of the living species is
+unknown.
+</p>`;
+
+posts.push(str);
+
+str = `
+<h1>Planet of the Apes</h1>
+<p>
+Planet of the Apes is an American science fiction media franchise
+consisting of films, books, television series, comics, and other media
+about a world in which humans and intelligent apes clash for
+control. The franchise is based on French author Pierre Boulle's 1963
+novel La Planète des singes, translated into English as Planet of the
+Apes or Monkey Planet. Its 1968 film adaptation, Planet of the Apes,
+was a critical and commercial hit, initiating a series of sequels,
+tie-ins, and derivative works. Arthur P. Jacobs produced the first
+five Apes films through APJAC Productions for distributor 20th Century
+Fox; since his death in 1973, Fox has controlled the franchise.
+</p>`;
+
+posts.push(str);
+
+str = `
+<h1>The Battle of Wesenberg (1268)</h1>
+<p>
+The Battle of Wesenberg, Rakvere or Rakovor was a battle fought on
+February 18, 1268, between the combined forces of Danish Duchy of
+Estonia, Bishopric of Dorpat, Livonian branch of the Teutonic Knights,
+and local Estonian militia on one side and the forces of Novgorod and
+Pskov, led by Dmitry of Pereslavl, on the other. Medieval accounts of
+the battle vary with both sides claiming victory, however the Livonian
+victory being more plausible as Novgorodian-Pskovian forces retreated
+out of Danish Estonia, with Livonian Knights launching a retaliatory
+attack on Izborsk and Pskov soon afterward, in June 1269.
+</p>`;
+
+posts.push(str);
+
+str = `
+<h1>The Barber of Seville</h1>
+<p>
+The Barber of Seville, or The Useless Precaution (Italian: Il barbiere
+di Siviglia, ossia L'inutile precauzione [il barˈbjɛːre di siˈviʎʎa
+osˈsiːa liˈnuːtile prekautˈtsjoːne]) is an opera buffa in two acts by
+Gioachino Rossini with an Italian libretto by Cesare Sterbini. The
+libretto was based on Pierre Beaumarchais's French comedy Le Barbier
+de Séville (1775). The première of Rossini's opera (under the title
+Almaviva, o sia L'inutile precauzione) took place on 20 February 1816
+at the Teatro Argentina, Rome, with designs by Angelo Toselli.
+</p>`;
+
+posts.push(str);
+
